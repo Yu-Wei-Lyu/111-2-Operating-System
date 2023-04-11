71be24066d6621c1699e80b47f1e6f5ef660012f
Yu-Wei, Lyu <awaia732@gmail.com>
2023-04-11 17:51:48 +0800

    Restruct file

---
+++ HW2/Teamwork/Chap4-P2/sorting.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+#define SIZE 10
+
+int list[SIZE] = {5, 8, 9, 7, 3, 1, 12, 25, 36, 13};
+int result[SIZE];
+
+typedef struct {
+    int *list;
+    int size;
+} sublist_t;
+
+int compare(const void *a, const void *b) {
+    int *x = (int *)a;
+    int *y = (int *)b;
+    return (*x - *y);
+}
+
+void *sort(void *arg) {
+    sublist_t *sublist = (sublist_t *)arg;
+    qsort(sublist->list, sublist->size, sizeof(int), compare);
+    pthread_exit(NULL);
+}
+
+void *merge(void *arg) {
+    int mid = SIZE / 2;
+    int i = 0, j = mid, k = 0;
+    while (i < mid && j < SIZE) {
+        if (list[i] < list[j]) {
+            result[k++] = list[i++];
+        } else {
+            result[k++] = list[j++];
+        }
+    }
+    while (i < mid) {
+        result[k++] = list[i++];
+    }
+    while (j < SIZE) {
+        result[k++] = list[j++];
+    }
+    pthread_exit(NULL);
+}
+
+int main() {
+    pthread_t threads[3];
+    sublist_t sublists[2];
+    sublists[0].list = list;
+    sublists[0].size = SIZE / 2;
+    sublists[1].list = list + (SIZE / 2);
+    sublists[1].size = SIZE / 2;
+    pthread_create(&threads[0], NULL, sort, &sublists[0]);
+    pthread_create(&threads[1], NULL, sort, &sublists[1]);
+    pthread_join(threads[0], NULL);
+    pthread_join(threads[1], NULL);
+    pthread_create(&threads[2], NULL, merge, NULL);
+    pthread_join(threads[2], NULL);
+    printf("Sorted list:\n");
+    for (int i = 0; i < SIZE; i++) {
+        printf("%d ", result[i]);
+    }
+    printf("\n");
+    return 0;
+}
